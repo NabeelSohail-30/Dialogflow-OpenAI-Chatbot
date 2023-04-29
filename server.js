a65082e476dac02a763b17cbc5c47f2c0a05e176
Nabeel <nabeel.sohail2630@outlook.com>
2023-04-29 08:20:03 +0500

    all set to deploy and test the basic server

--- server.js
+++ server.js
@@ -1,128 +1,94 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const axios = require('axios');
-const { v4: uuid } = require('uuid');
+const { Configuration, OpenAIApi } = require("openai");
+const cors = require('cors');
 const dialogflow = require('dialogflow');
 const dotenv = require('dotenv');
 
 dotenv.config();
-
 const app = express();
-const port = process.env.PORT || 5005;
-const token = process.env.TOKEN;
-const mytoken = process.env.MYTOKEN;
-const dialogflowProjectId = process.env.DIALOGFLOW_PROJECT_ID;
-
-const sessionClient = new dialogflow.SessionsClient();
-
+app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const port = process.env.PORT || 5005;
 
-//to verify the callback url from dashboard side - cloud api side
-app.get('/webhook', (req, res) => {
-    const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': verifyToken } = req.query;
-
-    if (mode && verifyToken) {
-        if (mode === 'subscribe' && verifyToken === mytoken) {
-            res.status(200).send(challenge);
-        } else {
-            res.sendStatus(403);
-        }
-    } else {
-        res.sendStatus(404);
-    }
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
-app.post('/webhook', async (req, res) => {
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const textGeneration = async (prompt) => {
     try {
-        const { entry } = req.body;
-
-        if (!entry || !entry[0] || !entry[0].changes || !entry[0].changes[0].value || !entry[0].changes[0].value.messages || !entry[0].changes[0].value.messages[0]) {
-            throw new Error('Invalid request body');
-        }
-
-        const { metadata, messages } = entry[0].changes[0].value;
-        const { phone_number_id: phoneNumberId } = metadata;
-        const { from, text: { body: messageBody } } = messages[0];
-
-        const sessionPath = sessionClient.sessionPath(dialogflowProjectId, uuid());
-
-        const request = {
-            session: sessionPath,
-            queryInput: {
-                text: {
-                    text: messageBody,
-                    languageCode: 'en-US',
-                },
-            },
-        };
-
-        const responses = await sessionClient.detectIntent(request);
-        const result = responses[0].queryResult;
-
-        const sendMessageUrl = `https://graph.facebook.com/v13.0/${phoneNumberId}/messages?access_token=${token}`;
-
-        await axios.post(sendMessageUrl, {
-            messaging_product: 'whatsapp',
-            to: from,
-            text: {
-                body: result.fulfillmentText,
-            },
+        const response = await openai.createCompletion({
+            model: 'text-davinci-003',
+            prompt: `Human: ${prompt}\nAI: `,
+            temperature: 0.9,
+            max_tokens: 500,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0.6,
+            stop: ['Human:', 'AI:']
         });
 
-        res.sendStatus(200);
+        return {
+            status: 1,
+            response: `${response.data.choices[0].text}`
+        };
     } catch (error) {
-        console.error('Error occurred while processing webhook request', error);
-        res.sendStatus(500);
+        return {
+            status: 0,
+            response: ''
+        };
     }
-});
+};
 
-// Dialogflow fulfillment webhook
-app.post('/dialogflow-fulfillment', async (req, res) => {
-    const intent = req.body.queryResult.intent.displayName;
+app.post('/webhook', async (req, res) => {
+    let intent = req.body.queryResult.intent.displayName;
+    let queryText = req.body.queryResult.queryText;
 
     switch (intent) {
         case 'Default Welcome Intent':
             {
-                res.send({
-                    fulfillmentMessages: [
-                        {
-                            text: {
-                                text: ['Welcome to the WhatsApp bot!'],
-                            },
-                        },
-                    ],
-                });
+                res.send(
+                    {
+                        fulfillmentMessages: [
+                            {
+                                text: {
+                                    text: [
+                                        `Hi, I'm your personal assistant. How can I help you?`
+                                    ]
+                                }
+                            }
+                        ]
+                    }
+                );
                 break;
             }
 
-        case 'About': {
-            res.send({
-                fulfillmentMessages: [
-                    {
-                        text: {
-                            text: ['This is a WhatsApp bot created using Dialogflow and Node.js.'],
-                        },
-                    },
-                ],
-            });
-            break;
-        }
-
         default: {
-            res.send({
-                fulfillmentMessages: [
+            let result = await textGeneration(queryText);
+            if (result.status == 1) {
+                res.send(
                     {
-                        text: {
-                            text: ['Sorry, I don\'t understand.'],
-                        },
-                    },
-                ],
-            });
+                        fulfillmentText: result.response
+                    }
+                );
+            } else {
+                res.send(
+                    {
+                        fulfillmentText: `Sorry, I'm not able to help with that.`
+                    }
+                );
+            }
         }
     }
 });
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server is up and listening on port ${port}`);
 });
