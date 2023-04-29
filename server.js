908e65ecd1f21b6c5b3ce2c26523cbddc7e168de
Nabeel <nabeel.sohail2630@outlook.com>
2023-04-29 22:58:11 +0500

    fine tuning data

--- server.js
+++ server.js
@@ -73,24 +73,29 @@ const generateText = async (prompt, fineTuneData) => {
 };
 
 // Fine-tune data
-const fineTuneData = `Human: Hello, who are you?
-AI: I am doing great. How can I help you today?
-Human: I am looking for a restaurant in the center of town.
-AI: Ok, what is the name of the restaurant?
-Human: The name of the restaurant is Blue Lemon.
-AI: Ok, what is the address, phone number and post code of the restaurant?
-Human: The address is 42 Church St. The phone number is 01223 244277. And the post code is CB2 3AR.
-AI: Here is the contact information for Blue Lemon restaurant. Address: 42 Church St. Phone number: 01223 244277. Post code: CB2 3AR.
-Human: Thank you. Can you tell me what type of food they serve and what is the price range?
-AI: They serve European food and the price range is moderate.
-Human: Can you give me the phone number of another restaurant?
-AI: Sure, what is the name of the restaurant?
-Human: The name of the restaurant is The Rice Boat.
-AI: Ok, what is the address, phone number and post code of the restaurant?
-Human: The address is 1 Riverside. The phone number is 01223 353554. And the post code is CB5 8JB.
-AI: Here is the contact information for The Rice Boat restaurant. Address: 1 Riverside. Phone number: 01223 353554. Post code: CB5 8JB.
-Human: Thank you. That is all I need.
-AI: You are welcome. Have a great day.`;
+const fineTuneData = `Saylani Welfare Trust is a non-profit organization based in Karachi, Pakistan. The organization was founded in 1999 by Maulana Bashir Ahmed Farooqi, a religious scholar, and social worker. The organization started as a small effort to provide meals to the needy, and it has now expanded to provide various services to underprivileged people.
+
+The organization is managed by a board of trustees, which is headed by Maulana Bashir Ahmed Farooqi. The board of trustees comprises of individuals from different fields, including religious scholars, businessmen, and professionals. The trustees oversee the overall functioning of the organization and ensure that the programs are implemented effectively.
+
+The organization has a dedicated team of professionals and volunteers who work tirelessly to provide services to the people. The team comprises of doctors, engineers, social workers, and other professionals who bring their expertise and skills to support the organization's programs.
+
+Saylani Welfare Trust has a transparent financial system and maintains detailed records of its finances. The organization receives funding from various sources, including donations from individuals and corporations, Zakat, and other charitable contributions. The organization ensures that the funds are utilized effectively and efficiently to support its programs.
+
+Over the years, Saylani Welfare Trust has received numerous awards and recognition for its services to the community. In 2015, the organization was awarded the Sitara-e-Imtiaz, one of the highest civil awards in Pakistan, for its contribution to social welfare.
+
+One of the significant programs run by Saylani Welfare Trust is the Saylani Mass Training and Job Placement Program.
+
+The Saylani Mass Training and Job Placement Program is designed to provide free technical and vocational training to unemployed youth, which will enable them to earn a decent livelihood. The program aims to provide training in different fields, such as computer programming, mobile phone repairing, and home appliance repairing. Once the training is completed, Saylani Welfare Trust helps these individuals to find suitable job opportunities.
+
+To support its training programs, Saylani Welfare Trust has partnered with a private educational institution in Karachi called the Sir Syed Institute of Technology (SMIT). SMIT is a technical college that offers courses in various fields, such as mechanical engineering, electrical engineering, and computer science. The college has state-of-the-art facilities and highly qualified faculty members who provide quality education to the students.
+
+Saylani Welfare Trust and SMIT have signed a Memorandum of Understanding (MoU) to collaborate on various programs, including the Saylani Mass Training and Job Placement Program. Under the MoU, SMIT provides training facilities and technical expertise, while Saylani Welfare Trust provides financial support and manages the overall program.
+
+The collaboration between Saylani Welfare Trust and SMIT has been a success, with thousands of students graduating from the training programs and finding employment opportunities. The program has not only provided much-needed support to the unemployed youth in Pakistan but has also helped in promoting technical and vocational education in the country.
+
+In addition to the Saylani Mass Training and Job Placement Program, Saylani Welfare Trust and SMIT also collaborate on other initiatives, such as providing scholarships to deserving students and organizing health camps for the underprivileged.
+
+In conclusion, Saylani Welfare Trust and SMIT have formed a strong partnership to promote technical and vocational education and provide job opportunities to the unemployed youth in Pakistan. The collaboration has been a success, and it is hoped that it will continue to benefit the people of Pakistan in the years to come.`;
 
 // Dialogflow webhook
 app.post('/webhook', async (req, res) => {
