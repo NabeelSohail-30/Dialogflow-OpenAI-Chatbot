fad5465af4ae1537f5d9faf9ebc9b4c2fea60e85
Nabeel <nabeel.sohail2630@outlook.com>
2023-04-30 03:40:20 +0500

    changing the logic

--- server.js
+++ server.js
@@ -25,52 +25,77 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-// fine tune the openai model on custom text and generate the answer for the questions
-const generateText = async (prompt) => {
+//fine tune the model
+const fineTune = async () => {
     try {
-        const fineTuneData = './data.txt'
-        if (!fineTuneData) {
-            throw new Error('Fine-tune data not provided');
-        }
+        const trainingData = [
+            `What is the capital of India? Delhi`,
+            `What is the capital of France? Paris`,
+            `What is the capital of Germany? Berlin`,
+            `What is the capital of Italy? Rome`,
+            `What is the capital of Spain? Madrid`,
+            `What is the capital of Japan? Tokyo`,
+            `What is the capital of China? Beijing`,
+            `What is the capital of Australia? Canberra`,
+            `What is the capital of USA? Washington DC`,
+            `What is the capital of Canada? Ottawa`,
+            `What is the capital of Russia? Moscow`,
+            `What is the capital of Brazil? Brasilia`,
+            `What is the capital of Argentina? Buenos Aires`,
+            `What is the capital of Mexico? Mexico City`,
+            `What is the capital of South Africa? Cape Town`,
+            `What is the capital of Nigeria? Abuja`,
+            `What is the capital of Egypt? Cairo`,
+            `What is the capital of Kenya? Nairobi`,
+            `What is the capital of Turkey? Ankara`,
+            `What is the capital of Iran? Tehran`,
+            `What is the capital of Iraq? Baghdad`,
+            `What is the capital of Saudi Arabia? Riyadh`,
+            `What is the capital of Pakistan? Islamabad`,
+            `What is the capital of Afghanistan? Kabul`,
+            `What is the capital of Nepal? Kathmandu`,
+            `What is the capital of Bhutan? Thimphu`,
+            `What is the capital of Bangladesh? Dhaka`,
+            `What is the capital of Sri Lanka? Colombo`,
+            `What is the capital of Myanmar? Naypyidaw`,
+        ];
 
-        let modelId;
-        const trainingData = {
-            file: fineTuneData,
-            model: 'davinci',
-            epoch: 3,
-            batchSize: 1,
-            learningRate: 0.0001,
-            prompt: 'Fine-tune the GPT model with your own data',
-            validationSplit: 0.1,
-            maxChars: 10000,
+        const fineTuneConfig = {
+            "prompt": "What is the capital of India? Delhi\nWhat is the capital of France? Paris\nWhat is the capital of Germany? Berlin\nWhat is the capital of Italy? Rome\nWhat is the capital of Spain? Madrid\nWhat is the capital of Japan? Tokyo\nWhat is the capital of China? Beijing\nWhat is the capital of Australia? Canberra\nWhat is the capital of USA? Washington DC\nWhat is the capital of Canada? Ottawa\nWhat is the capital of Russia? Moscow\nWhat is the capital of Brazil? Brasilia\nWhat is the capital of Argentina? Buenos Aires\nWhat is the capital of Mexico? Mexico City\nWhat is the capital of South Africa? Cape Town\nWhat is the capital of Nigeria? Abuja\nWhat is the capital of Egypt? Cairo\nWhat is the capital of Kenya? Nairobi\nWhat is the capital of Turkey? Ankara\nWhat is the capital of Iran? Tehran\nWhat is the capital of Iraq? Baghdad\nWhat is the capital of Saudi Arabia? Riyadh\nWhat is the capital of Pakistan? Islamabad\nWhat is the capital of Afghanistan? Kabul\nWhat is the capital of Nepal? Kathmandu\nWhat is the capital of Bhutan? Thimphu\nWhat is the capital of Bangladesh? Dhaka\nWhat is the capital of Sri Lanka? Colombo\nWhat is the capital of Myanmar? Naypyidaw\n",
+            "max_tokens": 1000,
+            "temperature": 0.3,
+            "top_p": 1,
+            "frequency_penalty": 0,
+            "presence_penalty": 0,
+            "stop": ["\n"]
         };
-        const response = await openaiClient.training.create(trainingData);
 
-        if (response.status !== 'success') {
-            throw new Error('Training failed');
-        }
-
-        modelId = response.data.model.id;
-
-        const responseChat = await openaiClient.completions.create({
-            engine: 'davinci',
-            prompt: `Human: ${prompt}\nAI: `,
-            maxTokens: 500,
-            n: 1,
-            stop: ['Human:', 'AI:'],
-            model: modelId,
-        });
-
-        return {
-            status: 1,
-            response: responseChat.data.choices[0].text,
-        };
+        const fineTuneResponse = await openaiClient.fineTunes.create(fineTuneConfig);
+        console.log(fineTuneResponse);
     } catch (error) {
-        console.error(`OpenAI API error: ${error}`);
-        return {
-            status: 0,
-            response: '',
+        console.error(`Fine tune error: ${error}`);
+    }
+};
+
+// Generate text
+const generateText = async (queryText) => {
+    try {
+        const generateConfig = {
+            "prompt": queryText,
+            "max_tokens": 100,
+            "temperature": 0.3,
+            "top_p": 1,
+            "frequency_penalty": 0,
+            "presence_penalty": 0,
+            "stop": ["\n"]
         };
+
+        const generateResponse = await openaiClient.completions.create(generateConfig);
+        console.log(generateResponse);
+        return { status: 1, response: generateResponse.data.choices[0].text };
+    } catch (error) {
+        console.error(`Generate text error: ${error}`);
+        return { status: 0, response: '' };
     }
 };
 
